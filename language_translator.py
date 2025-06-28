fb7b1f7f38c2b9c622a50f6ee4bbe0f53d312aad
KIRAN P <langotikiran1@gmail.com>
2025-06-28 20:01:50 +0530

    Update language_translator.py

--- language_translator.py
+++ language_translator.py
@@ -1,8 +1,7 @@
 from deep_translator import GoogleTranslator
 import streamlit as st
 
-langs = GoogleTranslator.get_supported_languages(as_dict=True)
-lang_names = list(langs.keys())
+langs = GoogleTranslator.get_supported_languages()
 
 st.title("🌐 Language Translation Tool")
 st.markdown("Built by **Kiran Langoti** – CodeAlpha AI Internship")
@@ -11,9 +10,9 @@ text = st.text_area("Enter text to translate")
 
 col1, col2 = st.columns(2)
 with col1:
-    source_lang = st.selectbox("Select source language", lang_names, index=lang_names.index("english"))
+    source_lang = st.selectbox("Select source language", langs, index=langs.index("english"))
 with col2:
-    target_lang = st.selectbox("Select target language", lang_names, index=lang_names.index("hindi"))
+    target_lang = st.selectbox("Select target language", langs, index=langs.index("hindi"))
 
 if st.button("Translate"):
     try:
@@ -22,4 +21,6 @@ if st.button("Translate"):
         st.text_area("Translated Text", translated, height=150)
     except Exception as e:
         st.error(f"Error: {e}")
+
+
 
